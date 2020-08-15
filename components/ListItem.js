c9c0bfeea47bc49ba341f35dd995c6f6a1ce7912
Girmay <ink423103@yahoo.com>
2020-08-15 22:16:07 +0200

    Button component and props added

--- components/ListItem.js
+++ components/ListItem.js
@@ -5,21 +5,61 @@ import {
   StyleSheet,
   TouchableOpacity,
   TextInput,
+  Button,
 } from 'react-native';
 //import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 const ListItem = ({
   item,
+  deleteItem,
+  editItem,
+  isEditing,
+  editItemDetail,
+  saveEditItem,
+  handleEditChange,
+  itemChecked,
+  checkedItems,
 }) => {
+  const checked = checkedItems.filter(
+    checkedItem => checkedItem.id === item.id,
+  );
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
-        <Text>
+      {isEditing && editItemDetail.id === item.id ? (
+          <TextInput
+            placeholder="Edit Item..."
+            style={styles.editItemInput}
+            onChangeText={handleEditChange}
+          />
+        ) : (
+          <Text
+            onPress={() => itemChecked(item.id, item.text)}
+            style={
+              checked.length ? styles.checkedItemText : styles.listItemText
+            }>
             {item.text}
-        </Text>
-        <Text>
-            Delete
-        </Text>
+          </Text>
+        )}
+        <View style={styles.iconView}>
+          {isEditing && editItemDetail.id === item.id ? (
+            <Button
+              name="save"
+              size={20}
+              color="green"
+              onPress={() => saveEditItem(item.id, item.text)}
+            />
+          ) : (
+            !checked.length && (
+              <Button
+                name="pencil"
+                size={20}
+                color="blue"
+                onPress={() => editItem(item.id, item.text)}
+              />
+            )
+          )}
+          </View>
       </View>
     </TouchableOpacity>
   );
