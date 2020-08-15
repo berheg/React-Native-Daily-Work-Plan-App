3aef11f4aff5848bbad6078b1d858bdb19a1f565
Girmay <ink423103@yahoo.com>
2020-08-15 22:11:48 +0200

    delete and edit functions added

--- App.js
+++ App.js
@@ -25,6 +25,39 @@ import ListItem from './components/ListItem';
     },
   ]);
 
+  // Flag true if user is currently editing an item
+  const [editStatus, editStatusChange] = useState(false);
+
+  // State to capture information about the item being edited
+  const [editItemDetail, editItemDetailChange] = useState({
+    id: null,
+    text: null,
+  });
+
+  const [checkedItems, checkedItemChange] = useState([]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
+
+  // Submit the users edits to the overall items state
+  const saveEditItem = (id, text) => {
+    setItems(prevItems => {
+      return prevItems.map(item =>
+        item.id === editItemDetail.id ? {id, text: editItemDetail.text} : item,
+      );
+    });
+    // Flip edit status back to false
+    editStatusChange(!editStatus);
+  };
+
+  // Event handler to capture users text input as they edit an item
+  const handleEditChange = text => {
+    editItemDetailChange({id: editItemDetail.id, text});
+  };
+
   const addItem = text => {
     if (!text) {
       Alert.alert(
@@ -44,6 +77,27 @@ import ListItem from './components/ListItem';
       });
     }
   };
+  // capture old items ID and text when user clicks edit
+  const editItem = (id, text) => {
+    editItemDetailChange({
+      id,
+      text,
+    });
+    return editStatusChange(!editStatus);
+  };
+
+  const itemChecked = (id, text) => {
+    const isChecked = checkedItems.filter(checkedItem => checkedItem.id === id);
+    isChecked.length
+      ? // remove item from checked items state (uncheck)
+        checkedItemChange(prevItems => {
+          return [...prevItems.filter(item => item.id !== id)];
+        })
+      : // Add item to checked items state
+        checkedItemChange(prevItems => {
+          return [...prevItems.filter(item => item.id !== id), {id, text}];
+        });
+  };
   return (
     <View style={styles.container}>
       <Header />
@@ -54,6 +108,14 @@ import ListItem from './components/ListItem';
         renderItem={({item}) => (
           <ListItem
             item={item}
+            deleteItem={deleteItem}
+            editItem={editItem}
+            isEditing={editStatus}
+            editItemDetail={editItemDetail}
+            saveEditItem={saveEditItem}
+            handleEditChange={handleEditChange}
+            itemChecked={itemChecked}
+            checkedItems={checkedItems}
           />
         )}
       />
