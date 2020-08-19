2f41f9a90c56cf12546fd8aaa0141ae3fed92b46
Girmay <ink423103@yahoo.com>
2020-08-19 23:10:54 +0200

    router name updated and commented notes removed

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
 const dailyPlanRouter = require("./api/dailyPlans");
-//const router = express.Router();
 
 const port = process.env.PORT || 5000;
-// For week4 no need to look into this!
-/* const path = require("path"); */
-/* // Serve the built client html
-const buildPath = path.join(__dirname, "../../dist");
-app.use(express.static(buildPath)); */
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Public folder setup
 app.use(express.static('public'));
-app.use("/api/dailyPlans", dailyPlansRouter);
+app.use("/api/dailyPlans", dailyPlanRouter);
 
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
